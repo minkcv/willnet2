81e62b0a253b4444d26a6bd1fb7de969c9bfef25
Will Smith <minkcv@gmail.com>
2014-09-22 21:22:45 -0500

    wireless gateway now only uses devices 0-100 to allow identity to be preserved in messages

---
+++ willnet2.lua
@@ -0,0 +1,10 @@
+rednet.open('back')
+print('Welcome to WillNet2. Type help for commands')
+io.write('willnet> ')
+cmd = io.read()
+while cmd ~= 'exit' do
+	if cmd == 
+	io.write('willnet> ')
+	cmd = io.read()
+end
+rednet.close('back')
--- wirelessgateway.lua
+++ wirelessgateway.lua
@@ -3,17 +3,19 @@ wired = peripheral.wrap('back')
 --important, channels could already be open before running this program
 wireless.closeAll()
 wired.closeAll()
---listen to everything
---note: this causes sender id to be lost
-wireless.open(65533)
-wired.open(65533)
+--listen to devices 0-100
+for i=0,100 do
+	wireless.open(i)
+	wired.open(i)
+end
 while true do
 	event, side, channel, replyChannel, msg, dist = os.pullEvent('modem_message')
 	print(msg.message)
-	print('from'..replyChannel)
+	print('from '..replyChannel)
+	print('to '..channel)
 	if side == 'back' then
-		wireless.transmit(65535, replyChannel, msg)
+		wireless.transmit(channel, replyChannel, msg)
 	elseif side == 'left' then
-		wired.transmit(65535, replyChannel, msg)
+		wired.transmit(channel, replyChannel, msg)
 	end
 end
